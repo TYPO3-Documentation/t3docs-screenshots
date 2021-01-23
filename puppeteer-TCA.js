3666d8aa9698702e8bc87141e83c9d84d43b7af3
linawolf <48202465+linawolf@users.noreply.github.com>
2021-01-23 22:40:10 +0100

    [Feature] Take simple Screenshots of certain fields

--- puppeteer-TCA.js
+++ puppeteer-TCA.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
     const page = await browser.newPage();
 
     // Set size of "browser window" - cannot click outside this area.
-    await page.setViewport({width: 1024, height: 768});
+    await page.setViewport({width: 640, height: 640});
 
     await page.goto('http://localhost');
     await page.waitForSelector('body');
@@ -66,8 +66,99 @@ const puppeteer = require('puppeteer');
     await page.screenshot({path: 'Screenshots/06_Fullpage_after_login.png', fullPage: true});
     await page.waitForTimeout(1000);
 
-    await page.goto('http://localhost/typo3/record/edit?token=9b3353c91c64fcab3c591ef82a3278558a6ff650&edit%5Btx_styleguide_inline_11%5D%5B1%5D=edit', {waitUntil: 'networkidle2'});
-    await page.screenshot({path: 'Screenshots/Screen09.png', fullPage: true});
+    let tcaFields = [
+        {
+            table: 'tx_styleguide_elements_basic',
+            path: 'Screenshots/Styleguide/',
+            uid: 7,
+            fields:
+                [
+                    'input_1',
+                    'input_30',
+                    'checkbox_2',
+                    "checkbox_3",
+                    "checkbox_12",
+                    "checkbox_16",
+                    "checkbox_17",
+                    "checkbox_18",
+                    "checkbox_19",
+                    "checkbox_21",
+                ]
+        },
+        {
+            table: 'tx_styleguide_inline_1n',
+            path: 'Screenshots/Styleguide/Inline1n',
+            uid: 1,
+            fields:
+                [
+                    'inline_1',
+                ]
+        },
+        {
+            table: 'tx_styleguide_inline_1n1n',
+            path: 'Screenshots/Styleguide/Inline1n1n',
+            uid: 1,
+            fields:
+                [
+                    'inline_1',
+                ]
+        },
+        {
+            table: 'tx_styleguide_inline_fal',
+            path: 'Screenshots/Styleguide/InlineFal',
+            uid: 1,
+            fields:
+                [
+                    'inline_1',
+                ]
+        },
+        {
+            table: 'tx_styleguide_inline_mn',
+            path: 'Screenshots/Styleguide/InlineMn',
+            uid: 1,
+            fields:
+                [
+                    'inline_1',
+                ]
+        },
+        {
+            table: 'tx_styleguide_inline_mnsymmetric',
+            path: 'Screenshots/Styleguide/InlineMnSymmetric',
+            uid: 11,
+            fields:
+                [
+                    'branches',
+                ]
+        },
+    ];
+
+    for (let i = 0; i < tcaFields.length; i++) {
+        for (let j = 0; j < tcaFields[i]['fields'].length; j++) {
+            let field = tcaFields[i]['fields'][j];
+            let filename =  toCamelCase(field);
+            await shootTCA(tcaFields[i]['table'], tcaFields[i]['uid'], field,
+                tcaFields[i]['path'] + filename + '.png');
+        }
+    }
+
+    async function shootTCA(table, uid, field, path) {
+        await page.goto('http://localhost/typo3/record/edit?token=9&edit[' + table + '][' + uid + ']=edit&columnsOnly=' + field, {waitUntil: 'networkidle2'});
+        const formSection = await page.$('.form-section');
+
+        console.log('Capturing: ' + path);
+        await formSection.screenshot({
+            path: path,
+        });
+    }
+
+    function toCamelCase(string) {
+        var splitStr = string.toLowerCase().split('_');
+        for (var k = 0; k < splitStr.length; k++) {
+            splitStr[k] = splitStr[k].charAt(0).toUpperCase() + splitStr[k].substring(1);
+        }
+        return splitStr.join('');
+    }
+
     /*
     const tree2 = await page.$(".identifier-0_8");
     await tree2.click();
