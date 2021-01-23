313006808e3594402ddf8f265a5e709ba2e65c9f
Thomas Anders <ta@dauskonzept.de>
2021-01-23 14:47:54 +0100

    modified screenshot script to safely run without faults Added Screenshots to check if git sees differences

--- puppeteer-testscript.js
+++ puppeteer-testscript.js
@@ -12,62 +12,60 @@ const puppeteer = require('puppeteer');
     // Screenshot: Full page of Frontend
     await page.screenshot({path: 'Screenshots/00_FE_Fullpage.png', fullPage: true});
 
-    await page.goto('http://localhost/typo3');
-    await page.waitForSelector('.typo3-login-logo');
+    // Switch to backend
+    await page.goto('http://localhost/typo3/login', {waitUntil: 'networkidle2'});
+    await page.waitForSelector('#loginCopyright');
 
     // Screenshot: Full page of Backend
     await page.screenshot({path: 'Screenshots/01_BE_Fullpage.png', fullPage: true});
 
     // 2nd shot: Login-Box
-    const loginWrap = await page.$('.card-login');
+    // await page.waitForTimeout(250);
+    const loginWrap = await page.$('div.card.card-lg.card-login');
     await loginWrap.screenshot({
         path: 'Screenshots/02_card-login.png',
     });
 
     // Screenshot: Add + 25px space around Login-Box
     const loginWrapBounds = await loginWrap.boundingBox();
-    const addSpace = 25;
     await loginWrap.screenshot({
         path: 'Screenshots/03_card_login_plus25.png',
         clip: {
-            x: loginWrapBounds.x - addSpace,
-            y: loginWrapBounds.y - addSpace,
-            width: loginWrapBounds.width + addSpace * 2,
-            height: loginWrapBounds.height + addSpace * 2
+            x: loginWrapBounds.x - 25,
+            y: loginWrapBounds.y - 25,
+            width: loginWrapBounds.width + 25 * 2,
+            height: loginWrapBounds.height + 25 * 2
         }
     });
 
-    //
-    // since here its working, now we need a specific user or the possibility to disable the login.
-    //
-
     // // 4th shot: Form-closeup after filling username
-    // const loginForm = await page.$('.panel-body');
-    // const loginFormFieldUsername = await page.$('#t3-username');
-    // await loginFormFieldUsername.type('demo-user');
-    // await loginForm.screenshot({
-    //   path: 'Screenshots/04_login_form.png'
-    // });
+    const loginForm = await page.$('.typo3-login-form');
+    const loginFormSubmitButton = await page.$('#t3-login-submit');
+    await page.type('#t3-username', 'pptr_admin');
+    await page.type('#t3-password', 'pptr_admin');
 
-    // // 5th shot: Form-closeup + 25px space around
-    // const loginFormBounds = await loginForm.boundingBox();
-    // await loginForm.screenshot({
-    //   path: 'Screenshots/05_login_form_plus_25.png',
-    //   clip: {
-    //     x: loginFormBounds.x - addSpace,
-    //     y: loginFormBounds.y - addSpace,
-    //     width: loginFormBounds.width + addSpace * 2,
-    //     height: loginFormBounds.height + addSpace * 2
-    //   }
-    // });
-    //
-    // await Promise.all([
-    //   page.waitForNavigation(),
-    //   loginFormFieldUsername.press('Enter')
-    // ]);
-    // await page.screenshot({path: 'Screenshots/06_Fullpage_after_login.png', fullPage: true});
-    // await page.waitForTimeout(1000);
-    // await page.screenshot({path: 'Screenshots/07_Fullpage_after_login_waited_1000ms.png', fullPage: true});
+    await loginForm.screenshot({
+        path: 'Screenshots/04_login_form.png'
+    });
+    const loginFormBounds = await loginForm.boundingBox();
+
+    await loginForm.screenshot({
+        path: 'Screenshots/05_login_form_plus50.png',
+        clip: {
+            x: loginFormBounds.x - 50,
+            y: loginFormBounds.y - 50,
+            width: loginFormBounds.width + 50 * 2,
+            height: loginFormBounds.height + 50 * 2
+        }
+    });
+
+    await Promise.all([
+        page.waitForNavigation(),
+        loginFormSubmitButton.click()
+    ]);
+    await page.screenshot({path: 'Screenshots/06_Fullpage_after_login.png', fullPage: true});
+    await page.waitForTimeout(1000);
+    await page.screenshot({path: 'Screenshots/07_Fullpage_after_login_waited_1000ms.png', fullPage: true});
 
     await browser.close();
 })()
