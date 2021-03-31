9c7b991b4ce46c44061c78bacb68bb20afe02d69
Alexander Nitsche <typo3@alexandernitsche.com>
2021-03-31 15:43:48 +0200

    [TASK] Make EXT:screenshots available for screenshot runner
    Clearly separate TYPO3 Context of browsable instance (Development)
    and screenshot runner (Testing) to disable the backend module
    for the screenshot runner so it does not appear in screenshots.

--- packages/screenshots/ext_tables.php
+++ packages/screenshots/ext_tables.php
@@ -2,17 +2,19 @@
 
 defined('TYPO3_MODE') or die();
 
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
-    'Screenshots',
-    'tools',
-    'screenshots',
-    '',
-    [
-        \TYPO3\CMS\Screenshots\Controller\ImageCopyController::class => 'index, make, compare, copy',
-    ],
-    [
-        'access' => 'user,group',
-        'icon'   => 'EXT:screenshots/Resources/Public/Icons/module-screenshots.svg',
-        'labels' => 'LLL:EXT:screenshots/Resources/Private/Language/locallang_mod.xlf',
-    ]
-);
+if (\TYPO3\CMS\Core\Core\Environment::getContext()->isTesting() === false) {
+    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerModule(
+        'Screenshots',
+        'tools',
+        'screenshots',
+        '',
+        [
+            \TYPO3\CMS\Screenshots\Controller\ImageCopyController::class => 'index, make, compare, copy',
+        ],
+        [
+            'access' => 'user,group',
+            'icon'   => 'EXT:screenshots/Resources/Public/Icons/module-screenshots.svg',
+            'labels' => 'LLL:EXT:screenshots/Resources/Private/Language/locallang_mod.xlf',
+        ]
+    );
+}
