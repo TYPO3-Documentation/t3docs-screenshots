71826cd44c1109b455a5dbb6ee10ae2f1c688b4f
Alexander Nitsche <typo3@alexandernitsche.com>
2021-05-06 21:49:10 +0200

    [FEATURE] Show number of selected screenshots in copy button

--- packages/screenshots/Resources/Public/JavaScript/ScreenshotsManager.js
+++ packages/screenshots/Resources/Public/JavaScript/ScreenshotsManager.js
@@ -9,11 +9,27 @@
 
 define(['jquery'], function ($) {
 
+  var ScreenshotsManager = {};
+
+  ScreenshotsManager.updateCopyButtonLabel = function() {
+    let numImagesToCopy = $('.images-to-copy').filter(':checked').length;
+    let numFilesToCopy = $('#screenshotsNumFiles').val();
+    let label = TYPO3.lang['button.copyscreenshots'].replace('%d', numImagesToCopy).replace('%d', numFilesToCopy);
+    $('.button-copy-screenshots').attr('value', label);
+  };
+
+  ScreenshotsManager.toggleAllImagesToCopy = function() {
+    let checked = $('#toggleImagesToCopy').prop('checked');
+    $('.images-to-copy').prop('checked', checked);
+  }
+
   $(document).ready(function () {
-    $('.smjs-toggle-all').on('click', function(){
-      let selector = $(this).data('toggle-selector');
-      let checked = $(this).prop('checked');
-      $(selector).prop('checked', checked);
+    $('.images-to-copy').on('click', function(){
+      ScreenshotsManager.updateCopyButtonLabel();
+    });
+    $('#toggleImagesToCopy').on('click', function(){
+      ScreenshotsManager.toggleAllImagesToCopy();
+      ScreenshotsManager.updateCopyButtonLabel();
     });
   });
 
