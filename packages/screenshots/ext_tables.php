b46b1a0d407bfaf9d31cfd95ac39b2ec70268747
Alexander Nitsche <typo3@alexandernitsche.com>
2021-05-07 13:50:52 +0200

    [FEATURE] Add searchbox to filter screenshots by url
    Add searchbox to filter screenshots by matching the url against
    substring or regular expression.
    
    Remove the "Copy" action and combine the handling in the "Compare"
    action to use the same form parameters without having to pass
    search terms etc. between the actions.

--- packages/screenshots/ext_tables.php
+++ packages/screenshots/ext_tables.php
@@ -9,7 +9,7 @@ if (\TYPO3\CMS\Core\Core\Environment::getContext()->isTesting() === false) {
         'screenshots',
         '',
         [
-            \TYPO3\CMS\Screenshots\Controller\ScreenshotsManagerController::class => 'index, make, compare, copy',
+            \TYPO3\CMS\Screenshots\Controller\ScreenshotsManagerController::class => 'index, make, compare',
         ],
         [
             'access' => 'user,group',
