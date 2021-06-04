2a4e3728f1fe1e0ff38c5bfbbf4f44ba812bfffd
Alexander Nitsche <typo3@alexandernitsche.com>
2021-06-04 11:24:34 +0200

    [FEATURE] Support extracting multiple members from class

--- packages/screenshots/Tests/Unit/Fixtures/ClassWithComments.php
+++ packages/screenshots/Tests/Unit/Fixtures/ClassWithComments.php
@@ -15,9 +15,19 @@ class ClassWithComments
     protected const CONSTANT_ONE = 'CONSTANT_ONE';
 
     /**
+     * This is constant CONSTANT_ONE_ONE.
+     */
+    protected const CONSTANT_ONE_ONE = 'CONSTANT_ONE_ONE';
+
+    /**
      * @var string Property One
      */
     protected string $propertyOne;
+
+    /**
+     * @var string Property One One
+     */
+    protected string $propertyOneOne;
 
     /**
      * @var string Property with default value
@@ -30,5 +40,21 @@ class ClassWithComments
     public function getPropertyOne(): string
     {
         return $this->propertyOne;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPropertyOneOne(): string
+    {
+        return $this->propertyOneOne;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPropertyWithDefaultValue(): string
+    {
+        return $this->propertyWithDefaultValue;
     }
 }
